ff90cfbcdf5a1326ef620d354fa86dd0e49822bc
fabian-bejarano <fabian.bejarano21@gmail.com>
2021-07-23 16:58:24 -0500

    clase 4

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) {
+
+        // DECALARACION E INICIALIZACION DEL ARREGLO "arr" con tamaño 5
+        // otro metodo int arr [] = {1,2,5,4,5};
+
+        int arr [] = new int [5];
+
+        arr [0] = 3;
+        arr [1] = 8;
+        arr [2] = 9;
+        arr [3] = 7;
+        arr [4] = 12;
         
     }
 }
