037806cea2d3e30b0ba176bf363065a80143f795
fabian-bejarano <fabian.bejarano21@gmail.com>
2021-07-26 21:21:26 -0500

    clase 04

--- src/App.java
+++ src/App.java
@@ -12,5 +12,8 @@ public class App {
         arr [3] = 7;
         arr [4] = 12;
         
+        for (int i = 0; i < arr.length; i ++){
+            System.out.println(arr [i]);
+        }
     }
 }
